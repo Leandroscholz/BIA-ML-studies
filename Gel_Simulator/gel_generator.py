c0f5db9f83c11416a17ed775ee5c6ff5d97dcdea
Leandro Aluisio Scholz <leandro.a.scholz@gmail.com>
2019-04-11 22:24:27 -0300

    add Gel_Simulator project

---
+++ Gel_Simulator/gel_generator.py
@@ -0,0 +1,18 @@
+import skimage
+import numpy as np 
+
+#def gel_generator(n_proteins, protein_velocities, time):
+    # """
+    # this function generates a synthetic image of a SDS-page 
+
+    # number of proteins
+    # protein velocities 
+    # time 
+
+    # """ 
+img_size = np.zeros((100,100))
+
+skimage.io.imsave('gel_image.png',img_size)
+
+
+
